3c44ae9025bc888e0cbd7d1ca8937258201cbe7e
Koushik <koushiknus@gmail.com>
2023-12-23 11:04:32 -0600

    New Project

---
+++ KotlinPractice/src/main/kotlin/HigherOrderFunctions.kt
@@ -0,0 +1,10 @@
+fun rollDice(numberOfTimes : Int , calculate : (result : Int) -> Unit ){
+
+    calculate(numberOfTimes*numberOfTimes)
+
+}
+fun main() {
+    rollDice(4,{result->
+        println(result)
+    })
+}
--- KotlinPractice/src/main/kotlin/PrimarySecondaryConstructors.kt
+++ KotlinPractice/src/main/kotlin/PrimarySecondaryConstructors.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Person{
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var firstName : String? = null
+    var lastName : String? = null
+
+    constructor(){
+        println("This is primary constructor")
+    }
+
+    constructor(firstName : String,lastName : String) : this(){
+        println("This is secondary constructor")
+
+    }
+
+    init {
+        println("This is first init block")
+    }
+    init {
+        println("This is second init block")
+    }
+}
+
+fun main(args: Array<String>){
+    val person = Person("test","test")
+
 }
